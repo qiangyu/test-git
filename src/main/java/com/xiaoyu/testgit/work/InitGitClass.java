f4dbc8b8a182454077cf284857aadd1de5ac8681
itqiangyu <z823631154>
2022-07-04 00:53:01 +0800

    首次提交commit

---
+++ src/main/java/com/xiaoyu/testgit/work/InitGitClass.java
@@ -0,0 +1,20 @@
+package com.xiaoyu.testgit.work;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.work
+ * @see InitGitClass
+ * @since 2022/7/4 0:50
+ */
+public class InitGitClass {
+
+    public static void main(String[] args) {
+        System.out.println("初始化git");
+    }
+
+}

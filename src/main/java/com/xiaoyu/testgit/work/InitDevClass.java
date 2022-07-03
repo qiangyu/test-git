95ba9a929b211169a4ad1838b99e3688b4f8013e
itqiangyu <z823631154>
2022-07-04 00:58:59 +0800

    首次提交dev分支commit

---
+++ src/main/java/com/xiaoyu/testgit/work/InitDevClass.java
@@ -0,0 +1,20 @@
+package com.xiaoyu.testgit.work;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.work
+ * @see InitDevClass
+ * @since 2022/7/4 0:58
+ */
+public class InitDevClass {
+
+    public static void main(String[] args) {
+        System.out.println("初始化dev分支");
+    }
+
+}

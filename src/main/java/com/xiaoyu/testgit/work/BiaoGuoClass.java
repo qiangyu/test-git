dce3486305aba84e9e20250acb2a5dc9f050b26d
itqiangyu <z823631154>
2022-07-04 14:27:40 +0800

    彪锅的垃圾代码

---
+++ src/main/java/com/xiaoyu/testgit/work/BiaoGuoClass.java
@@ -0,0 +1,18 @@
+package com.xiaoyu.testgit.work;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.work
+ * @see BiaoGuoClass
+ * @since 2022/7/4 13:59
+ */
+public class BiaoGuoClass {
+    public static void main(String[] args) {
+        System.out.println("彪锅写的垃圾代码");
+    }
+}

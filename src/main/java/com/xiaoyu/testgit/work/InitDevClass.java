15aedd279cad75d81c0f8d719ea51348b8997e1f
itqiangyu <z823631154>
2022-07-06 20:56:36 +0800

    完成dev分支开发

---
+++ src/main/java/com/xiaoyu/testgit/work/InitDevClass.java
@@ -0,0 +1,32 @@
+package com.xiaoyu.testgit.work;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.work
+ * @see InitDevClass
+ * @since 2022/7/4 0:58
+ */
+public class InitDevClass {
+
+    public static void main(String[] args) {
+        System.out.println("初始化dev分支");
+
+        System.out.println("添加登录功能");
+
+        System.out.println("添加注册功能");
+
+        System.out.println("添加发帖功能");
+
+        System.out.println("添加点赞功能");
+
+        System.out.println("添加评论功能");
+
+        System.out.println("添加收藏功能");
+    }
+
+}

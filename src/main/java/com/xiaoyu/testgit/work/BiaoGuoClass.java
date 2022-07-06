59fb4b804f5af0a358c7266f8a69f38917e66154
itqiangyu <z823631154>
2022-07-06 20:56:29 +0800

    彪锅写的bug

---
+++ src/main/java/com/xiaoyu/testgit/work/BiaoGuoClass.java
@@ -0,0 +1,20 @@
+package com.xiaoyu.testgit.work;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.work
+ * @see BiaoGuoClass
+ * @since 2022/7/4 13:59
+ */
+public class BiaoGuoClass {
+    public static void main(String[] args) {
+        System.out.println("彪锅写的垃圾代码");
+
+        System.out.println("新写的BUG");
+    }
+}

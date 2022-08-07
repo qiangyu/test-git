64c37898996f5e3cbd585c771e26408521cab629
qiangyu <823631154@qq.com>
2022-08-07 22:42:48 +0800

    Test sout -> create

---
+++ src/main/java/com/xiaoyu/testgit/test/Test.java
@@ -0,0 +1,20 @@
+package com.xiaoyu.testgit.test;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.test
+ * @see Test
+ * @since 2022/8/7 22:39
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("create");
+    }
+
+}

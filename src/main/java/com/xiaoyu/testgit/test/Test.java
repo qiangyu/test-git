ec17ebbe15d57edd9ba5b50dfe12caf6fc0dceec
qiangyu <823631154@qq.com>
2022-08-07 23:03:52 +0800

    create Test class

---
+++ src/main/java/com/xiaoyu/testgit/test/Test.java
@@ -0,0 +1,22 @@
+package com.xiaoyu.testgit.test;
+
+/**
+ * <p>
+ *
+ *
+ * </p>
+ *
+ * @author Xiao Yu
+ * @see com.xiaoyu.testgit.test
+ * @see Test
+ * @since 2022/8/7 22:39
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("create");
+
+        System.out.println("check");
+    }
+
+}
